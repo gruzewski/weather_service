ef390e35b4457a78ce9f4e188e1afaf94684cb2f
Jacek Gruzewski <jacek@broadbean.com>
2016-07-17 19:27:38 +0100

    Updated test for api endpoint

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,18 +1,33 @@
 import app
-import flask
+import vcr
 import unittest
+import urllib2
+
 
 class AppTestCase(unittest.TestCase):
+    my_vcr = vcr.VCR(
+        serializer='json',
+        cassette_library_dir='fixtures/vcr_cassettes',
+        record_mode='new_episodes',
+        match_on=['uri', 'method'],
+    )
+
 
     def setUp(self):
-        # Create Flas test client
+        # Create Flask test client
         self.app = app.app.test_client()
+        self.city = "London"
+        self.period = 3
 
-    def tearDown(self):
-        # Clear data
-        pass
 
     def test_hello_world(self):
         # Check main page
         response = self.app.get('/')
         assert 'Hello World!' in response.data
+
+
+    @my_vcr.use_cassette()
+    def test_api(self):
+        response = self.app.get("/api/v1.0/London/%s" % self.period)
+        self.assertEquals(response.status_code, 200)
+        assert 'humidity' in response.data
